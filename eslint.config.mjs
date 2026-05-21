fc29ce68df4a1960392e1223961bee4c92cdabd8
Alisensai <xnowalryu@gmail.com>
2026-05-21 15:13:55 +0300

    Add Prisma multi-tenant data foundation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = [
   {
-    ignores: ["node_modules/**", ".next/**", "out/**", "next-env.d.ts"]
+    ignores: ["node_modules/**", ".next/**", "out/**", "next-env.d.ts", "lib/generated/prisma/**"]
   },
   ...nextVitals
 ];
